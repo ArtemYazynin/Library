e59fc57177425b11dfcc3fe3db46a9083a10b9f8
artem <yazynindev@gmail.com>
2017-06-26 10:04:57 +0400

    Test. Source Tree check...

--- Library.ObjectModel/Models/Entity.cs
+++ Library.ObjectModel/Models/Entity.cs
@@ -1,9 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Library.ObjectModel.Models
 {
 	public class Entity
 	{
+		public Entity()
+		{
+			Version = DateTime.Now;
+		}
+
+		[Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public long Id { get; set; }
 		public DateTime Version { get; set; }
 	}
