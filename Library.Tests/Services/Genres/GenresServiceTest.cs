1a6075a9d3ebec350875622c03b50e422584350b
ArtemYazynin <yazynindev@gmail.com>
2017-11-13 09:57:55 +0400

    genres service test

--- Library.Tests/Services/Genres/GenresServiceTest.cs
+++ Library.Tests/Services/Genres/GenresServiceTest.cs
@@ -17,16 +17,28 @@ namespace Library.Tests.Services.Genres
 		}
 
 		[Test]
-		public void Delete_RootNodes_ShouldDeleteAllGenres()
+		public async Task Delete_RootNodes_ShouldDeleteAllGenres()
 		{
 			var rootGenres = Genres.Where(x=>x.Parent == null).ToList();
 
 			for (int i = 0, len=rootGenres.Count(); i < len; i++)
 			{
-				GenresService.Delete(rootGenres[i].Id, true);
+				await GenresService.Delete(rootGenres[i].Id, true);
 			}
 
 			Assert.That(Genres, Is.Empty);
+		}
+
+		[Test]
+		public async Task Delete_NodeWithNestedNodesRecurcivelly_ShouldDeleteNodeWithNestedNodes()
+		{
+			var genre = Genres.Single(x => x.Id == DefaultData.Genres.Programming.Id);
+
+			await GenresService.Delete(genre.Id, true);
+
+			var genres = Genres.Where(x => x.Parent?.Id == genre.Id);
+			Assert.That(genres.Where(x=>x.Id == genre.Id), Is.Empty);
+			Assert.That(genres, Is.Empty);
 		}
 	}
 }
